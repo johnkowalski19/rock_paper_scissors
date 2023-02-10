41d7dbe9798e7c60fc3491abe9c054050a5f65fb
John Kowalski <johnkowalski19@yahoo.com>
2023-02-10 15:54:45 -0700

    \ Write a function to play one round.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,36 @@ function getComputerChoice(){
     let randomIndex = Math.floor(Math.random()* 3);
     return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowercase();
+   if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
+        return 'You loose! Paper covers rock.';
+    } else if (computerSelection === 'scissors'){
+    return 'You win! Rock crushes scissors';
+   } else {
+    return "It's a tie!";
+   }
+}
+    if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+        return 'You win! Paper covers rock.'
+    } else if (computerSelection === 'scissors') {
+        return 'You loose! Scissors cut paper';
+    } else {
+        return "It's a tie!";
+    }
+}
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return 'You win! Scissors cut paper.';
+        }
+        else if (computerSelection === 'rock') {
+            return 'You loose! Rock crushes scissors.';
+        } else {
+            return "It's a tie!";
+        }
+    }
+}
+
