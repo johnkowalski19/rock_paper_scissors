b89e4c522e3b35d975c3018147fecde17632bc6f
John Kowalski <johnkowalski19@yahoo.com>
2023-02-11 16:54:10 -0700

    Start writing function game() Use a for loop and if statements.

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowercase();
    if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
-        return 'You loose! Paper covers rock.';
+        return 'You lose! Paper covers rock.';
     } else if (computerSelection === 'scissors'){
     return 'You win! Rock crushes scissors';
    } else {
@@ -19,7 +19,7 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'rock') {
         return 'You win! Paper covers rock.'
     } else if (computerSelection === 'scissors') {
-        return 'You loose! Scissors cut paper';
+        return 'You lose! Scissors cut paper';
     } else {
         return "It's a tie!";
     }
@@ -29,11 +29,37 @@ function playRound(playerSelection, computerSelection) {
             return 'You win! Scissors cut paper.';
         }
         else if (computerSelection === 'rock') {
-            return 'You loose! Rock crushes scissors.';
+            return 'You lose! Rock crushes scissors.';
         } else {
             return "It's a tie!";
         }
     }
 }
-
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+       let playerScore = prompt ('Enter you choice: Rock, Paper, or Scissors.')
+       if (playerSelection !== null) {
+        let computerSelection = getComputerChoice();
+       } 
+    }
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    if (result.startsWith('You Win!')) {
+        playerScore++;
+        
+    }
+    else if (result.startsWith('You Lose!')) {
+        computerScore++;
+    }
+ 
+    else {
+        console.log('You canceled the game.');
+    }
+    if (computerScore < playerScore) {
+        console.log('You Won the Game! Your score is ${playerscore} and the computer score is!{computerScore}. ');
+        
+    }
+}
 
